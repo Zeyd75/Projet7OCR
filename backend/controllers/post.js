17a8a2eff5b6b2f17aa9e9d7bb3b11e44b471c2b
Zeyd Haigar <zeyd.haigar@gmail.com>
2022-02-08 08:44:56 +0100

    suppression multer et fonctions inutiles

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -8,36 +8,6 @@ exports.create = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-exports.update = (req, res, next) => {
-  const sauceObject = req.file
-    ? {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
-  Sauce.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-};
-
-exports.delete = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      const filename = sauce.imageUrl.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-          .catch((error) => res.status(400).json({ error }));
-      });
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
-
 exports.getOne = (req, res, next) => {
   Post.findAll({
     include: [
